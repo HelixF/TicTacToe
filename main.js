e1fad4afa57bc28263d7931ad7cb4568e34468c4
Florian Soller <flsoller@gmail.com>
2020-06-04 22:32:12 +0200

    Basic grid and styling.

---
+++ main.js
@@ -0,0 +1,18 @@
+// Gameboard module
+const gameBoard = (() => {
+
+})();
+
+
+
+// Displaycontroller module
+const displayController = (() => {
+
+})();
+
+
+// Player factory
+const createPlayer = (name, mark) =>{
+
+    return { name, mark };
+}
