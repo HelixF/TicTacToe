4e94900368f30de74e3a51ab1b03333cd244994b
Florian Soller <flsoller@gmail.com>
2020-06-05 22:45:12 +0200

    Add event listeners to grid.

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 // Gameboard module
 const gameBoard = (() => {
 
-})();
+    // Array that represents the 3x3 grid. 
+    const boardArray = ['', '', '',
+                        '', '', '',
+                        '', '', ''];
 
+    return { boardArray };
+})();
 
 
 // Displaycontroller module
 const displayController = (() => {
+
+    // Adds event listeners to grid cells.
+    const board = document.getElementsByClassName('gridCell')
+    for (let i = 0; i < board.length; i++) {
+        board[i].addEventListener('click', ); //TODO link to Function that puts X or O 
+    }
 
 })();
 
@@ -16,3 +27,4 @@ const createPlayer = (name, mark) =>{
 
     return { name, mark };
 }
+
